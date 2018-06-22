a8bd9431eda3a109cf4a8d2f40fc3e5a7e0fb8cb
cforest1 <cforest@ulb.ac.be>
2018-06-22 14:33:36 +0200

    Ball speeds up

--- py_version/pong.py
+++ py_version/pong.py
@@ -11,7 +11,6 @@ Details:
 
 import pygame
 import sys
-import math
 from random import randint
 
 
@@ -23,14 +22,19 @@ class Ball(object):
         self.height = height                    # not important
         self.x_change = x_change
         self.y_change = y_change
+        self.speed = 1
         self.colour = colour
 
     def render(self, screen):                   # not important
         pygame.draw.ellipse(screen, self.colour, self.rect)
 
+    def accelerate(self):
+        if self.speed < 3:
+            self.speed += 0.2
+
     def update(self):
-        self.x += self.x_change
-        self.y += self.y_change
+        self.x += self.x_change * self.speed
+        self.y += self.y_change * self.speed
 
     @property
     def rect(self):                             # not important
@@ -102,6 +106,7 @@ class Pong(object):
 
     def play(self):
         clock = pygame.time.Clock()
+        pygame.time.set_timer(1, 5000)
         while True:
             clock.tick(50)
             for event in pygame.event.get():
@@ -113,6 +118,9 @@ class Pong(object):
                         self.player1.key_handler(event)
                     # elif event.joy == 1:
                         self.player2.key_handler(event)
+                if event.type == 1:
+                    print("speeding up")
+                    self.ball.accelerate()
             self.collision_handler()
             self.draw()
 
@@ -130,9 +138,9 @@ class Pong(object):
             sys.exit()
 
         if self.ball.y + self.ball.height >= self.screen.get_height():
-            self.ball.y_change = -math.fabs(self.ball.y_change)
+            self.ball.y_change = -abs(self.ball.y_change)
         elif self.ball.y <= 0:
-            self.ball.y_change = math.fabs(self.ball.y_change)
+            self.ball.y_change = abs(self.ball.y_change)
 
 #        if self.player1.y + self.player1.height > self.screen.get_height():
 #            self.player1.y = self.screen.get_height() - self.player1.height
